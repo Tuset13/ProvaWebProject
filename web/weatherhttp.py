f0461fe8aa8c2c5e34a272e3e6ed4270a8457335
Tuset13 <user01@alcatel13.udl.net>
2019-02-26 09:28:31 +0100

    JSON dic

--- web/weatherhttp.py
+++ web/weatherhttp.py
@@ -1,31 +1,39 @@
 from urllib.request import urlopen
 import bs4
+import xmltodict
+import json
+import pprint
 
 class WeatherClient(object):
     """Veure el temps que ens ofereix OpenWeather"""
     def __init__(self):
         super(WeatherClient, self).__init__()
-        pass
 
     def do_request(self):
-        f = urlopen("https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&mode=xml&appid=928c5cf213ebd781ade87ca8cbfeeee6")
+        f = urlopen("https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&mode=json&appid=928c5cf213ebd781ade87ca8cbfeeee6")
         data = f.read()
         f.close()
         return data
 
-    def process_weather(self, html):
-        arbre = bs4.BeautifulSoup(html, features='lxml')
-        temperature = arbre.find("temperature")
-        weather = arbre.find("weather")
-        print(temperature["value"] + " and " + weather["value"])
+#    def process_weather(self, html):
+#        arbre = bs4.BeautifulSoup(html, features='lxml')
+#        temperature = arbre.find("temperature")
+#        weather = arbre.find("weather")
+#        return(temperature["value"] + " and " + weather["value"])
 
-        return None
+    def process_weather(self, html):
+        dic = json.loads(html)
+        pprint.pprint(dic)
+        temp = dic['list'][0]['main']['temp']
+        weath = dic['list'][0]['weather'][0]['description']
+        return (str(temp) + " and " + weath)
 
     def run(self):
         #Descarregar-me html
         data = self.do_request()
         data2 = self.process_weather(data)
         #print(infoLlibre)
+        print(data2 )
 
 if __name__ == "__main__":
     w = WeatherClient()
