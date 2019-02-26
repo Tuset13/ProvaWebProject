5ba1d9a30c2d9eb0fcb82cab9c38d77b4e9f07a9
Tuset13 <user01@alcatel13.udl.net>
2019-02-26 10:45:48 +0100

    New strange things

--- web/Django/paquipalla/envelopes/models.py
+++ web/Django/paquipalla/envelopes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length = 100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Envelope(models.Model):
@@ -13,5 +13,5 @@ class Envelope(models.Model):
     motive = models.CharField(max_length = 100)
     company = models.ForeignKey(Company, on_delete = models.CASCADE)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.company.name + " " + self.motive
